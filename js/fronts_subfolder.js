2893b8f6a28370e61850a814a762b0e83a474aed
luqmanthinkpad <luqmanthinkpad@gmail.com>
2026-03-02 08:25:16 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- js/fronts_subfolder.js
+++ js/fronts_subfolder.js
@@ -102,7 +102,7 @@
                 </section>
                 <footer class="footer pt20 pb20 bgcolor-gray" style="border-top: 1px solid #eee; margin-top: 30px;">
                     <div class="container text-center">
-                        <p style="font-size: 12px; color: #777;">&copy; 2026 Newsgo. All rights reserved.</p>
+                        <p style="font-size: 12px; color: #777;">&copy; All rights reserved.</p>
                     </div>
                 </footer>
             </div>
@@ -370,3 +370,4 @@
     }
 
 })();
+
